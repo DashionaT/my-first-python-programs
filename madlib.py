b831905f05bdc3f579f6fff797ccb291612332e9
DashionaT <dtodd5092@greenvilleschools.us>
2017-08-30 09:13:54 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,17 @@
+
+print("Give me a noun.")
+noun1 = input()
+
+print("Give me a action verb.")
+action_verb1 = input()
+
+print("Give me a adjective?")
+adjective1 = input()
+
+print(" Give me a noun.")
+noun2= input()
+
+print (" Give me a place.")
+noun3 = input()
+
+print( noun1 + " was " + action_verb1 + " with the " + adjective1 + " " + noun2 + " to " + noun3 + ".")
